db5b7ea5b94e15618f9ebe94e1a7db23c4140dcb
ramon <ramonrodriguesnw@gmail.com>
2022-02-23 04:09:31 -0300

    nomeclatura de variaveis mais compreensiveis

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const img = document.getElementById('carousel');
-const rightBtn = document.getElementById('right-btn');
-const leftBtn = document.getElementById('left-btn');
+const direitaBTN = document.getElementById('btn-direita');
+const esquerdaBTN = document.getElementById('btn-esquerda');
 
 // Imagens upada em unsplash.com
-let pictures = 
+let imagens = 
 [
     'https://i.postimg.cc/bvcp9gXs/jonathan-borba-6r-Onmv-EOKg-U-unsplash.jpg',
     'https://i.postimg.cc/cCHSL535/jonathan-borba-92dr-Kkzne-P4-unsplash.jpg',
@@ -11,31 +11,31 @@ let pictures =
     'https://i.postimg.cc/8zyW98C7/bailey-burton-s-QKHf-Nl-Uw9-U-unsplash.jpg'
 ];
 
-img.src = pictures[0];
+img.src = imagens[0];
 let position = 0;
 
 // Passar Imagens a direita ao clicar no botão da direita
-const moveRight = () => {
-    if (position >= pictures.length - 1) {
+const nextdireita = () => {
+    if (position >= imagens.length - 1) {
         position = 0
-        img.src = pictures[position];
+        img.src = imagens[position];
         return;
     }
-    img.src = pictures[position + 1];
+    img.src = imagens[position + 1];
     position++;
 }
 
 // Passar Imagens a Esquerda ao clicar no botão da Esjquerda
-const moveLeft = () => {
+const nextesquerda = () => {
     if (position < 1) {
-        position = pictures.length - 1;
-        img.src = pictures[position];
+        position = imagens.length - 1;
+        img.src = imagens[position];
         return;
     }
-    img.src = pictures[position - 1];
+    img.src = imagens[position - 1];
     position--;
 }
 
 // Evento que liga os botões a função de passar a direita ou esquerda
-rightBtn.addEventListener("click", moveRight);
-leftBtn.addEventListener("click", moveLeft);
+direitaBTN.addEventListener("click", nextdireita);
+esquerdaBTN.addEventListener("click", nextesquerda);

1cf548b9826a6371629a57f248dfbfa64799332f
ramon <ramonrodriguesnw@gmail.com>
2022-02-25 00:07:39 -0300

    Formataçao de arquivos selecionados

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ const direitaBTN = document.getElementById('btn-direita');
 const esquerdaBTN = document.getElementById('btn-esquerda');
 
 // Imagens upada em unsplash.com
-let imagens = 
-[
+let imagens = [
     'https://i.postimg.cc/bvcp9gXs/jonathan-borba-6r-Onmv-EOKg-U-unsplash.jpg',
     'https://i.postimg.cc/cCHSL535/jonathan-borba-92dr-Kkzne-P4-unsplash.jpg',
     'https://i.postimg.cc/ncGqK9bY/234666a572046af02a9d6d8763387963.jpg',
